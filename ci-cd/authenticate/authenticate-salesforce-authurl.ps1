704e61f65d4c28857337d9439cac6ecf8e079f43
jdschleicher <jdschleicher@gmail.com>
2022-11-28 21:49:27 -0500

    setting devhub default

--- ci-cd/authenticate/authenticate-salesforce-authurl.ps1
+++ ci-cd/authenticate/authenticate-salesforce-authurl.ps1
@@ -7,7 +7,7 @@ $key_file = "auth_url.key"
 New-Item -Type File $key_file | Out-Null
 $auth_url | Out-File $key_file
 
-$auth_result_json = $(sfdx auth:sfdxurl:store -f $key_file -a $org_alias --json)
+$auth_result_json = $(sfdx auth:sfdxurl:store -f $key_file -a $org_alias -d --json)
 Write-Host $auth_result_json
 $auth_result = $auth_result_json| ConvertFrom-Json
 $auth_result.status
